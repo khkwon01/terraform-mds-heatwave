938b87fc5246afb4de93b0ce7d2d88db6d84c8c6
khkwon01 <khkwon01@gmail.com>
2022-11-03 11:52:43 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-data "oci_identity_tenancy" "tenancy" {
-  tenancy_id = var.tenancy_ocid  
-}
-
-# get the tenancy's home region
-data "oci_identity_regions" "home_region" {
-  filter {
-    name   = "key"
-    values = ["YNY"]
-  }
-}
 
 provider oci {
 	region = var.region
@@ -16,5 +5,5 @@ provider oci {
 
 provider oci {
 	alias  = "home"
-	region = lookup(data.oci_identity_regions.home_region.regions[0], "name")
+	region = "ap-chuncheon-1"
 }
