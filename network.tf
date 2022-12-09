3ac2c67d345c88b756d529e88051abec62ffc890
khkwon01 <khkwon01@gmail.com>
2022-12-09 15:32:10 +0900

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,111 +1,72 @@
-resource oci_core_vcn oke-vcn {
+resource oci_core_vcn mds-vcn {
   cidr_block     = "10.0.0.0/16"
   compartment_id = var.compartment_ocid
   defined_tags = {
   }
-  display_name = "oke-vcn"
+  display_name = "mds-vcn"
   dns_label    = "ocidevcluster"
   freeform_tags = {
   }
-  
 }
 
-resource oci_core_internet_gateway oke-igw {
+resource oci_core_internet_gateway mds-igw {
   compartment_id = var.compartment_ocid
   defined_tags = {
   }
-  display_name = "oke-igw"
+  display_name = "mds-igw"
   enabled      = "true"
   freeform_tags = {
   }
-  vcn_id = oci_core_vcn.oke-vcn.id
+  vcn_id = oci_core_vcn.mds-vcn.id
 }
 
-resource oci_core_subnet oke-subnet-regional {
-  
-  cidr_block     = "10.0.10.0/24"
-  compartment_id = var.compartment_ocid
-  defined_tags = {
-  }
-  dhcp_options_id = oci_core_vcn.oke-vcn.default_dhcp_options_id
-  display_name    = "oke-subnet-regional"
-  dns_label       = "okesubnet"
-  freeform_tags = {
-  }
-  
-  prohibit_public_ip_on_vnic = "false"
-  route_table_id             = oci_core_vcn.oke-vcn.default_route_table_id
-  security_list_ids = [
-    oci_core_security_list.oke-seclist.id,
-  ]
-  vcn_id = oci_core_vcn.oke-vcn.id
-}
-resource oci_core_subnet oke-svclbsubnet-regional {
+resource oci_core_subnet mds-subnet-regional {
   
   cidr_block     = "10.0.20.0/24"
   compartment_id = var.compartment_ocid
   defined_tags = {
   }
-  dhcp_options_id = oci_core_vcn.oke-vcn.default_dhcp_options_id
-  display_name    = "oke-svclbsubnet-regional"
-  dns_label       = "okelbsubnet"
-  freeform_tags = {
-  }
-  
-  prohibit_public_ip_on_vnic = "false"
-  route_table_id             = oci_core_vcn.oke-vcn.default_route_table_id
-  security_list_ids = [
-    oci_core_vcn.oke-vcn.default_security_list_id,
-  ]
-  vcn_id = oci_core_vcn.oke-vcn.id
-}
-resource oci_core_subnet mds-subnet-regional {
-  
-  cidr_block     = "10.0.30.0/24"
-  compartment_id = var.compartment_ocid
-  defined_tags = {
-  }
-  dhcp_options_id = oci_core_vcn.oke-vcn.default_dhcp_options_id
+  dhcp_options_id = oci_core_vcn.mds-vcn.default_dhcp_options_id
   display_name    = "mds-subnet-regional"
   dns_label       = "mdssubnet"
   freeform_tags = {
   }
   
   prohibit_public_ip_on_vnic = "true"
-  route_table_id             = oci_core_vcn.oke-vcn.default_route_table_id
+  route_table_id             = oci_core_vcn.mds-vcn.default_route_table_id
   security_list_ids = [
     oci_core_security_list.mds-seclist.id,
   ]
-  vcn_id = oci_core_vcn.oke-vcn.id
+  vcn_id = oci_core_vcn.mds-vcn.id
 }
 
 resource oci_core_subnet operator-subnet-regional {
   
-  cidr_block     = "10.0.40.0/24"
+  cidr_block     = "10.0.10.0/24"
   compartment_id = var.compartment_ocid
   defined_tags = {
   }
-  dhcp_options_id = oci_core_vcn.oke-vcn.default_dhcp_options_id
+  dhcp_options_id = oci_core_vcn.mds-vcn.default_dhcp_options_id
   display_name    = "operator-subnet-regional"
   dns_label       = "operatorsubnet"
   freeform_tags = {
   }
   
   prohibit_public_ip_on_vnic = "false"
-  route_table_id             = oci_core_vcn.oke-vcn.default_route_table_id
+  route_table_id             = oci_core_vcn.mds-vcn.default_route_table_id
   security_list_ids = [
     oci_core_security_list.operator-seclist.id,
   ]
-  vcn_id = oci_core_vcn.oke-vcn.id
+  vcn_id = oci_core_vcn.mds-vcn.id
 }
 
-resource oci_core_default_dhcp_options Default-DHCP-Options-for-oke-vcn {
+resource oci_core_default_dhcp_options Default-DHCP-Options-for-mds-vcn {
   defined_tags = {
   }
-  display_name = "Default DHCP Options for oke-vcn"
+  display_name = "Default DHCP Options for mds-vcn"
   freeform_tags = {
   }
-  manage_default_resource_id = oci_core_vcn.oke-vcn.default_dhcp_options_id
+  manage_default_resource_id = oci_core_vcn.mds-vcn.default_dhcp_options_id
   options {
     custom_dns_servers = [
     ]
@@ -122,113 +83,22 @@ resource oci_core_default_dhcp_options Default-DHCP-Options-for-oke-vcn {
     type = "SearchDomain"
   }
 }
-resource oci_core_default_route_table oke-routetable {
+  
+resource oci_core_default_route_table mds-routetable {
   defined_tags = {
   }
-  display_name = "oke-routetable"
+  display_name = "mds-routetable"
   freeform_tags = {
   }
-  manage_default_resource_id = oci_core_vcn.oke-vcn.default_route_table_id
+  manage_default_resource_id = oci_core_vcn.mds-vcn.default_route_table_id
   route_rules {
     
     destination       = "0.0.0.0/0"
     destination_type  = "CIDR_BLOCK"
-    network_entity_id = oci_core_internet_gateway.oke-igw.id
+    network_entity_id = oci_core_internet_gateway.mds-igw.id
   }
 }
-resource oci_core_security_list oke-seclist {
-  compartment_id = var.compartment_ocid
-  defined_tags = {
-  }
-  display_name = "oke-seclist"
 
-  egress_security_rules {
-    
-    destination      = "0.0.0.0/0"
-    destination_type = "CIDR_BLOCK"
-    
-    protocol  = "all"
-    stateless = "false"
-    
-  }
-  freeform_tags = {
-  }
-  ingress_security_rules {
-    
-    protocol    = "all"
-    source      = "10.0.10.0/24"
-    source_type = "CIDR_BLOCK"
-    stateless   = "true"
-    
-  }
-  ingress_security_rules {
-    
-    protocol    = "all"
-    source      = "10.0.20.0/24"
-    source_type = "CIDR_BLOCK"
-    stateless   = "true"
-    
-  }
-  ingress_security_rules {
-    
-    protocol    = "all"
-    source      = "10.0.40.0/24"
-    source_type = "CIDR_BLOCK"
-    stateless   = "true"
-    
-  }
-  ingress_security_rules {
-    
-    icmp_options {
-      code = "4"
-      type = "3"
-    }
-    protocol    = "1"
-    source      = "0.0.0.0/0"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
-    
-  }
-  ingress_security_rules {
-    
-    protocol    = "6"
-    source      = "0.0.0.0/0"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
-    tcp_options {
-      max = "22"
-      min = "22"
-      
-    }
-    
-  }
-  vcn_id = oci_core_vcn.oke-vcn.id
-}
-resource oci_core_default_security_list oke-svclbseclist {
-  defined_tags = {
-  }
-  display_name = "oke-svclbseclist"
-  egress_security_rules {
-    
-    destination      = "0.0.0.0/0"
-    destination_type = "CIDR_BLOCK"
-    
-    protocol  = "6"
-    stateless = "true"
-    
-  }
-  freeform_tags = {
-  }
-  ingress_security_rules {
-    
-    protocol    = "6"
-    source      = "0.0.0.0/0"
-    source_type = "CIDR_BLOCK"
-    stateless   = "true"
-    
-  }
-  manage_default_resource_id = oci_core_vcn.oke-vcn.default_security_list_id
-}
 resource oci_core_security_list mds-seclist {
   compartment_id = var.compartment_ocid
   defined_tags = {
@@ -256,22 +126,6 @@ resource oci_core_security_list mds-seclist {
   }
   ingress_security_rules {
     
-    protocol    = "all"
-    source      = "10.0.20.0/24"
-    source_type = "CIDR_BLOCK"
-    stateless   = "true"
-    
-  }
-  ingress_security_rules {
-    
-    protocol    = "all"
-    source      = "10.0.40.0/24"
-    source_type = "CIDR_BLOCK"
-    stateless   = "true"
-    
-  }
-  ingress_security_rules {
-    
     icmp_options {
       code = "4"
       type = "3"
@@ -282,7 +136,7 @@ resource oci_core_security_list mds-seclist {
     stateless   = "false"
     
   }
-  vcn_id = oci_core_vcn.oke-vcn.id
+  vcn_id = oci_core_vcn.mds-vcn.id
 }
 
 resource oci_core_security_list operator-seclist {
@@ -327,6 +181,6 @@ resource oci_core_security_list operator-seclist {
     }
     
   }
-  vcn_id = oci_core_vcn.oke-vcn.id
+  vcn_id = oci_core_vcn.mds-vcn.id
 }
 
