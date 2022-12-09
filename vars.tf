88967e9cf2c3965eb0aa0b91cbb4c90d04fa436b
khkwon01 <khkwon01@gmail.com>
2022-12-09 14:32:34 +0900

    Update vars.tf
    modify default value

--- vars.tf
+++ vars.tf
@@ -1,18 +1,10 @@
-#identity
+#identity  => you should modify this value according to your test environment
 variable compartment_ocid { }
 variable tenancy_ocid { default = "ocid1.tenancy.oc1..aaaaaaaa4yet6xypq5khquqhozb7rja2cwomj4zvpxvqgusrnojr3t5rieiq" }
 variable region { default = "ap-chuncheon-1" }
 
 #network
 
-
-#oke
-variable kube_cluster_name { default = "oke_cluster" }
-variable kube_version { default = "v1.21.5" }
-variable node_shape { default = "VM.Standard.E4.Flex" }
-variable node_shape_ocpus { default = 1 }
-variable node_shape_memory { default = 8 }
-variable node_pool_ssh_public_key { default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvRZ6JUfI6qGFL5Y3Ql2/9Awr3stdOTUTK2dDbvppG0f8gSACQHK6qdJkuQMESRdaTlxhBAxBxBB46C6d9v2bYSroyNhGPf3Nk2vqaV5Sg75krHqnT4UTnRaTxGj3xj6xtpBsVFABIAK7fNwrrWvza+8MLyX83rwUGlm8CPoI5a32+EUuNEiOzWnPRCtuh+rnpozyRgEQyVD/r0Y/rQIwQVrvdTn2XziIkJ8gxGn39FtswUArwuo/iJ306WXaMxYzTpVVhiw+dzKPOoCz4R0D9PsuxL3EtZeXSi8B8di2Og5caURu4E5j3lsBvUkD/mUD3JTZap9KGImVjfM7B3/zn"}
 
 #operator
 variable operator_shape { default = "VM.Standard.E4.Flex" }
@@ -21,7 +13,7 @@ variable operator_shape_memory { default = 8 }
 variable operator_ssh_public_key { default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvRZ6JUfI6qGFL5Y3Ql2/9Awr3stdOTUTK2dDbvppG0f8gSACQHK6qdJkuQMESRdaTlxhBAxBxBB46C6d9v2bYSroyNhGPf3Nk2vqaV5Sg75krHqnT4UTnRaTxGj3xj6xtpBsVFABIAK7fNwrrWvza+8MLyX83rwUGlm8CPoI5a32+EUuNEiOzWnPRCtuh+rnpozyRgEQyVD/r0Y/rQIwQVrvdTn2XziIkJ8gxGn39FtswUArwuo/iJ306WXaMxYzTpVVhiw+dzKPOoCz4R0D9PsuxL3EtZeXSi8B8di2Og5caURu4E5j3lsBvUkD/mUD3JTZap9KGImVjfM7B3/zn"}
 variable operator_ssh_private_key_path { default = "./keys/id_rsa"}
 variable operator_os { default = "Oracle Linux" }
-variable operator_os_version { default = "7.9" }
+variable operator_os_version { default = "8.6" }
 
 # MySQL Data Service
 variable "mds_instance_name" {
@@ -37,16 +29,14 @@ variable "mysql_db_system_admin_username" {
 variable "mysql_db_system_admin_password" {
   description = "Password for the admin user for MySQL Database Service"
   type        = string
-  default     = "Oracle#123"
+  default     = "Welcome#1"
 }
 
 variable "mysql_shape_name" {
-    #default = "MySQL.VM.Standard.E3.1.8GB"
     default = "MySQL.HeatWave.VM.Standard.E3"
 }
 
 variable "mysql_data_storage_in_gb" {
-    #default = 50
     default = 1024
 }
 
