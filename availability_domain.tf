bf84ae054aa3cf6d46a6818796d6fa4ffee498f9
khkwon01 <khkwon01@gmail.com>
2022-12-09 14:07:47 +0900

    Update availability_domain.tf

--- availability_domain.tf
+++ availability_domain.tf
@@ -4,6 +4,7 @@ data oci_identity_availability_domain AD-1 {
   compartment_id = var.compartment_ocid
   ad_number      = "1"
 }
+
 #data oci_identity_availability_domain AD-2 {
 #  compartment_id = var.compartment_ocid
 #  ad_number      = "2"
