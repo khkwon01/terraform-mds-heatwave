aa52327eb7275e190dd9a2fbaece3ceeac485447
khkwon01 <khkwon01@gmail.com>
2023-04-21 15:38:19 +0900

    Update mds.tf

--- mds.tf
+++ mds.tf
@@ -16,6 +16,6 @@ resource "oci_mysql_mysql_configuration" "MDSinstance_configuration" {
     shape_name = var.mysql_shape_name
     display_name = var.mysql_db_configuration
     variables {
-        sql_generate_invisible_primary_key = "ON"
+        sql_require_primary_key = "ON"
     }
 }
