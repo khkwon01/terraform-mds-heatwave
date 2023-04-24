7baddbdbcd4ff03180d354ebd45ce051ced8047b
khkwon01 <khkwon01@gmail.com>
2023-04-24 20:59:07 +0900

    Update install_web_php_template.sh

--- scripts/install_web_php_template.sh
+++ scripts/install_web_php_template.sh
@@ -12,6 +12,8 @@ sudo dnf module install php:7.4 -y
 sudo yum install php-cli php-mysqlnd php-zip php-gd php-mbstring php-xml php-json -y
 sudo systemctl restart httpd
 
+sudo setsebool -P httpd_can_network_connect 1
+
 sudo echo "
 <?php
 phpinfo();
@@ -36,6 +38,5 @@ echo 'Successfull Connect.';
 ?>
 " > /var/www/html/config.php
 
-sudo setsebool -P httpd_can_network_connect 1
 
 echo "The apache and php successfully installed !"
