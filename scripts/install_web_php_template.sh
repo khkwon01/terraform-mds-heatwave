0fd4eaa34d34c30a235510c7f1811179973934c1
khkwon01 <khkwon01@gmail.com>
2023-04-24 20:58:46 +0900

    Update install_web_php_template.sh

--- scripts/install_web_php_template.sh
+++ scripts/install_web_php_template.sh
@@ -12,11 +12,29 @@ sudo dnf module install php:7.4 -y
 sudo yum install php-cli php-mysqlnd php-zip php-gd php-mbstring php-xml php-json -y
 sudo systemctl restart httpd
 
-sudo echo '
+sudo echo "
 <?php
 phpinfo();
 ?>
-' > /var/www/html/info.php
+" > /var/www/html/info.php
+
+sudo echo "
+<?php
+// Database credentials
+define('DB_SERVER', '10.0.20.128');// MDS server IP address
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'Welcome#1');
+define('DB_NAME', 'census');
+//Attempt to connect to MySQL database
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// Check connection
+if($link === false){
+die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+// Print database connection result
+echo 'Successfull Connect.';
+?>
+" > /var/www/html/config.php
 
 sudo setsebool -P httpd_can_network_connect 1
 
