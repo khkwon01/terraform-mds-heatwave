f86ce8684daec3665130f6a59a906e06dc319b5a
khkwon01 <khkwon01@gmail.com>
2023-04-24 22:15:57 +0900

    Update install_web_php_template.sh

--- scripts/install_web_php_template.sh
+++ scripts/install_web_php_template.sh
@@ -28,10 +28,10 @@ define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'Welcome#1');
 define('DB_NAME', 'census');
 //Attempt to connect to MySQL database
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+\$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
-if($link === false){
-die("ERROR: Could not connect. " . mysqli_connect_error());
+if(\$link === false){
+die('ERROR: Could not connect. ' . mysqli_connect_error());
 }
 // Print database connection result
 echo 'Successfull Connect.';
