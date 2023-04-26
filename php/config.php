ca8d667404d69cf6d48a4e8fead731db7f6bd1cb
khkwon01 <khkwon01@gmail.com>
2023-04-26 23:14:00 +0900

    Create config.php

---
+++ php/config.php
@@ -0,0 +1,15 @@
+<?php
+// Database credentials
+define('DB_SERVER', '10.0.20.40');// MDS server IP address
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'Welcome#1');
+define('DB_NAME', 'census');
+//Attempt to connect to MySQL database
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// Check connection
+if($link === false){
+die('ERROR: Could not connect. ' . mysqli_connect_error());
+}
+// Print database connection result
+// echo 'Successfull Connect.';
+?>
