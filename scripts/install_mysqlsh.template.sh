4c112078472cd2f4211016bf3f62b9c57a7a0bb9
khkwon01 <khkwon01@gmail.com>
2023-06-16 14:00:51 +0900

    Update install_mysqlsh.template.sh

--- scripts/install_mysqlsh.template.sh
+++ scripts/install_mysqlsh.template.sh
@@ -10,4 +10,7 @@ sudo echo '{
 }' | sudo tee -a ~${user}/.mysqlsh/options.json
 sudo chown -R ${user} ~${user}/.mysqlsh
 
+# download for tpc-h data
+sudo wget https://drive.google.com/file/d/1K-ZqxSZ_paxTFIYAFCxfqdcagvUjmy3g/view?usp=sharing
+
 echo "MySQL Shell successfully installed !"
